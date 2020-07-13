8ca59d8fbf87a6cd301f3e8f4994a541e9f2eda6
bender <bender.d1mon@gmail.com>
2020-07-13 22:31:41 +0300

    promisify example

---
+++ docs/child-processes/exec/exec-promisify.js
@@ -0,0 +1,10 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function lsExample() {
+  const { stdout, stderr } = await exec('ls');
+  console.log('stdout:', stdout);
+  console.error('stderr:', stderr);
+}
+
+lsExample();
